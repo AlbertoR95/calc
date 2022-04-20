d9e90c67dd35315b93e767f9acf176bfa4fa6c53
Alberto Rapisarda <albertorapisarda27@gmail.com>
2022-04-20 20:21:19 +0100

    Select mode

--- EasyCalculator/src/Calculator.java
+++ EasyCalculator/src/Calculator.java
@@ -13,10 +13,19 @@ public class Calculator {
         System.out.println("Integer insert i");
         String choose = sc.next().toLowerCase();
 
+        while(!choose.equals("i") & !choose.equals("f")) {
+            System.out.println("Ops");
+            System.out.println("Float insert f");
+            System.out.println("");
+            System.out.println("Integer insert i");
+            choose = sc.next().toLowerCase();
+        }
+
         if (choose.equals("i")) {
             i.letStart();
         } else if (choose.equals("f")) {
             f.letStart();
         }
+
     }
 }
